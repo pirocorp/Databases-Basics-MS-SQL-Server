4ed2302385aa2d753e2aca3d95628ade50fc6715
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-21 17:49:53 +0100

    P17_BackupDatabase

---
+++ 01. Data Definition and Datatypes/Exercises/P17_BackupDatabase/BackupDatabase.sql
@@ -0,0 +1,5 @@
+BACKUP DATABASE SoftUni  
+TO DISK='D:\source\repos\Databases-Basics-MS-SQL-Server\01. Data Definition and Datatypes\Exercises\P17_BackupDatabase\softuni-backup.bak'
+WITH FORMAT,  
+   MEDIANAME = 'SoftUni',  
+   MEDIADESCRIPTION = 'Striped media set for SoftUni database';
---
+++ 01. Data Definition and Datatypes/Exercises/P17_BackupDatabase/RestoreDatabase.sql
@@ -0,0 +1,2 @@
+RESTORE DATABASE SoftUni  
+   FROM DISK = 'D:\source\repos\Databases-Basics-MS-SQL-Server\01. Data Definition and Datatypes\Exercises\P17_BackupDatabase\softuni-backup.bak' 
