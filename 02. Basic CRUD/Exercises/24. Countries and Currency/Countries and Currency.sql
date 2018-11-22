1a89b7a3ef8c03d1ac24d2026081a610e8476faf
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-22 18:16:29 +0100

    24. Countries and Currency

---
+++ 02. Basic CRUD/Exercises/24. Countries and Currency/Countries and Currency.sql
@@ -0,0 +1,10 @@
+  SELECT 
+	     CountryName, 
+		 CountryCode,
+		 Currency =
+					CASE CurrencyCode
+					WHEN 'EUR' THEN 'Euro'
+					ELSE 'Not Euro'
+					END
+    FROM Countries
+ORDER BY CountryName ASC
