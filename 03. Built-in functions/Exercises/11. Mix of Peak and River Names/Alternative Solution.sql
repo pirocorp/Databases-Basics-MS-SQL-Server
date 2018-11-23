0b6b79deafd3058750eacc6b76ffeafe010bc53c
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-23 20:09:56 +0100

    Alternative Solution

--- 03. Built-in functions/Exercises/11. Mix of Peak and River Names/Alternative Solution.sql
+++ 03. Built-in functions/Exercises/11. Mix of Peak and River Names/Alternative Solution.sql
@@ -4,7 +4,8 @@ USE [Geography]
   SELECT PeakName, 
 		 RiverName,
 		 LOWER(CONCAT(PeakName, SUBSTRING(RiverName, 2, LEN(RiverName)))) AS [Mix]
-    FROM Peaks, Rivers
+    FROM Peaks, 
+		 Rivers
    WHERE RIGHT(Peaks.PeakName, 1) = LEFT(Rivers.RiverName, 1)
 ORDER BY Mix
 	  GO
