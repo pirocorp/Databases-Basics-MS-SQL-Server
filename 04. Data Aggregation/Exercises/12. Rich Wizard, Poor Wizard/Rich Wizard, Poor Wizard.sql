e2bc643141c7d4313a343774d21cef8f62a715b9
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-25 18:12:55 +0100

    12. Rich Wizard, Poor Wizard

---
+++ 04. Data Aggregation/Exercises/12. Rich Wizard, Poor Wizard/Rich Wizard, Poor Wizard.sql
@@ -0,0 +1,10 @@
+--CTE Solution
+  WITH Wizards AS(SELECT FirstName AS [Host Wizard],
+                  	     DepositAmount AS [Host Wizard Deposit],
+                  	     LEAD(FirstName, 1) OVER (ORDER BY Id) AS [Guest Wizard],
+                  	     LEAD(DepositAmount, 1) OVER (ORDER BY Id) AS [Guest Wizard Deposit],
+                  	     DepositAmount - LEAD(DepositAmount, 1) OVER (ORDER BY Id) AS [Difference]
+                    FROM WizzardDeposits)
+SELECT SUM([Difference]) AS SumDifference
+  FROM Wizards
+    GO
