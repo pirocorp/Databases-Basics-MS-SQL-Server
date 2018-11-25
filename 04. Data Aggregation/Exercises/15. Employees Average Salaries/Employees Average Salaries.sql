d63e21063dca9b92b2ae4b7723875dcc45d16202
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-25 18:13:29 +0100

    15. Employees Average Salaries

---
+++ 04. Data Aggregation/Exercises/15. Employees Average Salaries/Employees Average Salaries.sql
@@ -0,0 +1,31 @@
+--CTE Solution NULL != 42 evaluates to false (evaluates to null so result is skipped)
+	WITH EmployeeSalary
+      AS (SELECT DepartmentID,
+                 CASE  
+                     WHEN  DepartmentID = 1 THEN 5000 + Salary
+          		     ELSE Salary
+                 END AS [Salary],
+          	     ManagerID
+            FROM Employees
+           WHERE Salary > 30000 AND (ManagerID != 42 OR ManagerID IS NULL))
+  SELECT DepartmentID,
+         AVG(Salary) AS AverageSalary
+    FROM EmployeeSalary
+GROUP BY DepartmentID
+
+--Standart Solution
+--This create ne table and copy all entities from employees for given condition
+SELECT * INTO [EmployeesAS] FROM Employees
+WHERE [Salary] > 30000
+
+DELETE FROM EmployeesAS
+WHERE [ManagerID] = 42
+ 
+UPDATE EmployeesAS
+SET [Salary] += 5000
+WHERE [DepartmentID] = 1
+ 
+SELECT [DepartmentID],
+    AVG([Salary]) as [AverageSalary]
+FROM EmployeesAS
+GROUP BY [DepartmentID]
