8d118ffb6eec6d7954a672d4ffcf200f1743f4b4
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-27 16:45:58 +0100

    05. Online Store Database

---
+++ 05. Table Relations/Exercises/05. Online Store Database/Online Store Database.sql
@@ -0,0 +1,70 @@
+CREATE TABLE Cities(
+	CityID INT IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_Cities
+	PRIMARY KEY(CityID),
+)
+
+CREATE TABLE Customers(
+	CustomerID INT IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	Birthday DATE,
+	CityID INT,
+
+	CONSTRAINT PK_Customers
+	PRIMARY KEY(CustomerID),
+
+	CONSTRAINT FK_Customers_CityID_Cities_CityID
+	FOREIGN KEY(CityID)
+	REFERENCES Cities(CityID),
+)
+
+CREATE TABLE Orders(
+	OrderID INT IDENTITY,
+	CustomerID INT,
+
+	CONSTRAINT PK_Orders
+	PRIMARY KEY(OrderID),
+
+	CONSTRAINT FK_Orders_CustomerID_Customers_CustomerID
+	FOREIGN KEY(CustomerID)
+	REFERENCES Customers(CustomerID),
+)
+
+CREATE TABLE ItemTypes(
+	ItemTypeID INT IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_ItemTypes
+	PRIMARY KEY(ItemTypeID),
+)
+
+CREATE TABLE Items(
+	ItemID INT IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	ItemTypeID INT,
+
+	CONSTRAINT PK_Items
+	PRIMARY KEY(ItemID),
+
+	CONSTRAINT FK_Items_ItemTypeID_ItemTypes_ItemTypeID
+	FOREIGN KEY(ItemTypeID)
+	REFERENCES ItemTypes(ItemTypeID),
+)
+
+CREATE TABLE OrderItems(
+	OrderID INT,
+	ItemID INT,
+
+	CONSTRAINT PK_OrderItems
+	PRIMARY KEY(OrderID, ItemID),
+
+	CONSTRAINT FK_OrderItems_OrderID_Orders_OrderID
+	FOREIGN KEY(OrderID)
+	REFERENCES Orders(OrderID),
+
+	CONSTRAINT FK_OrderItems_ItemID_Items_ItemID
+	FOREIGN KEY(ItemID)
+	REFERENCES Items(ItemID)
+)
