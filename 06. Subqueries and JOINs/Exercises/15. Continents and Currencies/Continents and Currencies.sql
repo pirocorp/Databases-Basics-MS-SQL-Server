9fac2625102d7a11f09f20b218323e5e4822d6b1
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-29 12:45:28 +0100

    formatting

--- 06. Subqueries and JOINs/Exercises/15. Continents and Currencies/Continents and Currencies.sql
+++ 06. Subqueries and JOINs/Exercises/15. Continents and Currencies/Continents and Currencies.sql
@@ -3,8 +3,8 @@ SELECT ContinentCode,
 	   CurrencyUsage
   FROM (  SELECT ContinentCode,
                  CurrencyCode,
-        		  COUNT(CurrencyCode) AS CurrencyUsage,
-        		  DENSE_RANK() OVER(PARTITION BY ContinentCode ORDER BY COUNT(CurrencyCode) DESC) AS CurrencyRank
+        		 COUNT(CurrencyCode) AS CurrencyUsage,
+        		 DENSE_RANK() OVER(PARTITION BY ContinentCode ORDER BY COUNT(CurrencyCode) DESC) AS CurrencyRank
             FROM Countries
         GROUP BY ContinentCode, 
                  CurrencyCode) 
