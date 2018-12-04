2c28e28b15e39b33a429577601cb8eaa35b2cfd7
Zdravko Zdravkov <pirocorp@abv.bg>
2018-12-04 20:03:02 +0100

    09. Find Full Name

---
+++ 07. Database Programmability and Transactions/Exercises/09. Find Full Name/Find Full Name.sql
@@ -0,0 +1,8 @@
+CREATE OR ALTER PROC usp_GetHoldersFullName
+                  AS
+			  SELECT CONCAT(FirstName, ' ', LastName) AS [Full Name]
+			    FROM AccountHolders
+				  GO
+
+EXEC usp_GetHoldersFullName
+  GO
