a8fc7ff5685baf2ccbf6327bb99e78d7c4e5a02f
Zdravko Zdravkov <pirocorp@abv.bg>
2018-12-04 20:02:37 +0100

    07. Define Function

---
+++ 07. Database Programmability and Transactions/Exercises/07. Define Function/Define Function.sql
@@ -0,0 +1,26 @@
+CREATE OR ALTER FUNCTION ufn_IsWordComprised(@SetOfChars NVARCHAR(MAX), @InputWord NVARCHAR(MAX))
+             RETURNS BIT 
+                      AS
+                   BEGIN 
+	         			 DECLARE @CurrentChar NVARCHAR(1)
+	         			 DECLARE @Word NVARCHAR(Max) = @InputWord
+						   WHILE (@SetOfChars != '')
+						   BEGIN
+								   SET @CurrentChar = RIGHT(@SetOfChars, 1)
+	         					    IF (charindex(@CurrentChar, @Word) > 0)
+	         					 BEGIN
+								       SET @Word = REPLACE(@Word, @CurrentChar, '')--stuff(@Word, charindex(@CurrentChar, @Word), 1, '')
+	         					   END
+								   SET @SetOfChars = SUBSTRING(@SetOfChars, 1, LEN(@SetOfChars) - 1)
+							 END
+	         			 DECLARE @Result BIT
+						  SELECT @Result = CASE   
+						                   WHEN LEN(@Word) > 0 THEN 0  
+						                   ELSE 1 
+						               END
+						  RETURN @Result   
+                     END 
+	         	      GO
+
+SELECT dbo.ufn_IsWordComprised('oistmiahf', 'Shammi')
+    GO
