5af25883fd32eb8147e34dc504c100f923cbe395
Zdravko Zdravkov <pirocorp@abv.bg>
2018-12-04 20:04:39 +0100

    17. Withdraw Money Procedure

---
+++ 07. Database Programmability and Transactions/Exercises/17. Withdraw Money Procedure/Withdraw Money Procedure.sql
@@ -0,0 +1,30 @@
+CREATE OR ALTER PROC usp_WithdrawMoney (@AccountId INT, @MoneyAmount DECIMAL(15, 4)) 
+                  AS 
+				  IF (@MoneyAmount > 0)
+			   BEGIN 
+			         DECLARE @CurrentAmount DECIMAL(15, 4) = (SELECT Balance
+                                                                FROM Accounts
+                                                               WHERE Id = @AccountId)
+			              IF (@CurrentAmount - @MoneyAmount >= 0)
+					   BEGIN
+					         UPDATE Accounts
+							    SET Balance -= @MoneyAmount
+							  WHERE Id = @AccountId
+					     END                      
+			     END
+				  GO
+
+BEGIN TRANSACTION
+EXEC usp_WithdrawMoney 1, 133.1199
+
+SELECT Balance
+  FROM Accounts
+ WHERE Id = 1
+
+ROLLBACK
+      GO
+
+SELECT Balance
+  FROM Accounts
+ WHERE Id = 1
+    GO
