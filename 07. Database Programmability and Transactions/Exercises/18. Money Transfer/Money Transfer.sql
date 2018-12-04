76766d8adfea9c0e383a6f4a4560c3102d6ca22f
Zdravko Zdravkov <pirocorp@abv.bg>
2018-12-04 20:04:51 +0100

    18. Money Transfer

---
+++ 07. Database Programmability and Transactions/Exercises/18. Money Transfer/Money Transfer.sql
@@ -0,0 +1,33 @@
+CREATE OR ALTER PROC usp_TransferMoney(@SenderId INT, @ReceiverId INT, @Amount DECIMAL(15, 4))
+                  AS 
+   BEGIN TRANSACTION
+                     EXEC usp_WithdrawMoney @SenderId, @Amount
+					    IF @@ROWCOUNT <> 1
+					 BEGIN
+				           ROLLBACK
+						   RAISERROR('Transfer Failed', 16, 1)
+						   RETURN
+					   END
+					  EXEC usp_DepositMoney @ReceiverId, @Amount
+					  	IF @@ROWCOUNT <> 1
+					 BEGIN
+				           ROLLBACK
+						   RAISERROR('Transfer Failed', 16, 2)
+						   RETURN
+					   END
+					COMMIT
+				  GO
+
+BEGIN TRANSACTION
+SELECT * 
+  FROM Accounts
+ WHERE Id IN (1, 2)
+
+EXEC usp_TransferMoney 1, 2, 10
+
+SELECT * 
+  FROM Accounts
+ WHERE Id IN (1, 2)
+
+ROLLBACK
+  GO
