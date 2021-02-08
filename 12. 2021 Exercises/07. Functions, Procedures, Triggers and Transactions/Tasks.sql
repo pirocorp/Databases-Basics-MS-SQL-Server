26423753ed449c57f1c92ee181bf18319ed018de
Zdravko Zdravkov <zdr0@yahoo.com>
2021-02-08 16:16:33 +0100

    Update

--- 12. 2021 Exercises/07. Functions, Procedures, Triggers and Transactions/Tasks.sql
+++ 12. 2021 Exercises/07. Functions, Procedures, Triggers and Transactions/Tasks.sql
@@ -296,3 +296,413 @@ GO
 DROP FUNCTION ufn_CashInUsersGames
 GO
 
+-- Task 14
+USE Bank
+GO
+
+CREATE TABLE Logs(
+	LogId INT PRIMARY KEY IDENTITY,
+	AccountId INT,
+	OldSum DECIMAL(14, 2),
+	NewSum DECIMAL(14, 2)
+);
+GO
+
+CREATE TRIGGER tr_AddToLogsOnAccountUpdate
+ON Accounts FOR UPDATE
+AS
+	INSERT INTO [Logs]
+	SELECT [d].[Id] AS [AccountId]
+		  ,[d].[Balance] AS [OldSum]
+		  ,[i].[Balance] AS [NewSum]
+	  FROM [deleted] AS [d]
+	  JOIN [inserted] AS [i]
+		ON [d].[Id] = [i].[Id]
+GO
+
+DROP TRIGGER tr_AddToLogsOnAccountUpdate
+GO
+
+-- Task 15
+CREATE TRIGGER tr_AddToLogsOnAccountUpdate
+ON Accounts FOR UPDATE
+AS
+	INSERT INTO [Logs]
+	SELECT [d].[Id] AS [AccountId]
+		  ,[d].[Balance] AS [OldSum]
+		  ,[i].[Balance] AS [NewSum]
+	  FROM [deleted] AS [d]
+	  JOIN [inserted] AS [i]
+		ON [d].[Id] = [i].[Id]
+GO
+
+CREATE TABLE NotificationEmails(
+	[Id] INT PRIMARY KEY IDENTITY,
+	[Recipient] INT,
+	[Subject] NVARCHAR(MAX),
+	[Body] NVARCHAR(MAX)
+);
+GO
+
+CREATE TRIGGER tr_AddNotificationEmailOnAccountUpdate
+ON Logs FOR INSERT
+AS
+	INSERT INTO [NotificationEmails]
+	SELECT [AccountId] AS [RecipientId]
+		  ,CONCAT('Balance change for account: ', [AccountId]) AS [Subject]
+		  ,CONCAT('On ', CONVERT(varchar, GETDATE(), 0), ' your balance was changed from ', [OldSum], ' to ', [NewSum], '.') AS [Body]
+	  FROM [inserted]
+GO
+
+SELECT * 
+  FROM [Logs]
+
+SELECT * 
+  FROM [NotificationEmails]
+
+SELECT * FROM Accounts
+
+UPDATE [Accounts]
+   SET [Balance] += 1000
+ WHERE [AccountHolderId] = 2
+    GO
+
+SELECT * 
+  FROM [Logs]
+
+SELECT * 
+  FROM [NotificationEmails]
+
+DROP TRIGGER tr_AddNotificationEmailOnAccountUpdate
+GO
+
+DROP TRIGGER tr_AddToLogsOnAccountUpdate
+GO
+
+-- Task 16
+CREATE PROCEDURE usp_DepositMoney (@AccountId INT, @MoneyAmount DECIMAL(16, 4)) AS
+IF @MoneyAmount > 0
+	UPDATE [Accounts]
+	   SET [Balance] += @MoneyAmount
+	 WHERE [Accounts].[Id] = @AccountId
+GO
+
+SELECT * FROM Accounts WHERE Id = 1
+
+EXEC dbo.usp_DepositMoney 1, 10
+
+SELECT * FROM Accounts WHERE Id = 1
+
+DROP PROCEDURE usp_DepositMoney
+GO
+
+-- Task 17
+CREATE PROCEDURE usp_WithdrawMoney (@AccountId INT, @MoneyAmount DECIMAL(16, 4)) AS
+IF @MoneyAmount > 0
+	UPDATE [Accounts]
+	   SET [Balance] -= @MoneyAmount
+	 WHERE [Accounts].[Id] = @AccountId
+GO
+
+DROP PROCEDURE usp_WithdrawMoney
+GO
+
+-- Task 18
+CREATE PROCEDURE usp_DepositMoney (@AccountId INT, @MoneyAmount DECIMAL(16, 4)) AS
+IF @MoneyAmount > 0
+	UPDATE [Accounts]
+	   SET [Balance] += @MoneyAmount
+	 WHERE [Accounts].[Id] = @AccountId
+GO
+
+CREATE PROCEDURE usp_WithdrawMoney (@AccountId INT, @MoneyAmount DECIMAL(16, 4)) AS
+IF @MoneyAmount > 0
+	UPDATE [Accounts]
+	   SET [Balance] -= @MoneyAmount
+	 WHERE [Accounts].[Id] = @AccountId
+GO
+
+CREATE PROCEDURE usp_TransferMoney(@SenderId INT, @ReceiverId INT, @Amount DECIMAL(16, 4)) AS
+	BEGIN TRANSACTION
+		IF @Amount < 0
+		BEGIN
+			ROLLBACK
+			RETURN
+		END
+		
+	EXEC dbo.usp_DepositMoney @ReceiverId, @Amount
+	EXEC dbo.usp_WithdrawMoney @SenderId, @Amount			
+	COMMIT
+GO
+
+SELECT * FROM Accounts WHERE Id IN (1, 5)
+GO
+
+EXEC usp_TransferMoney 1, 5, 5000
+
+SELECT * FROM Accounts WHERE Id IN (1, 5)
+GO
+
+DROP PROCEDURE usp_DepositMoney
+GO
+
+DROP PROCEDURE usp_WithdrawMoney
+GO
+
+DROP PROCEDURE usp_TransferMoney
+GO
+-- Task 19
+USE Diablo
+GO
+
+CREATE TRIGGER tr_UserGameItemsUpdate
+ON [UserGameItems] INSTEAD OF INSERT
+AS
+	INSERT INTO [UserGameItems]
+	SELECT [UGI].[ItemId]
+		  ,[UGI].[UserGameId]
+	  FROM [inserted] AS [UGI]
+	  JOIN [Items] AS [I]
+        ON [UGI].[ItemId] = [I].[Id]
+      JOIN [UsersGames] AS [UG]
+        ON [UGI].[UserGameId] = [UG].[Id]
+	 WHERE [I].[MinLevel] <= [UG].[Level]
+GO
+
+INSERT INTO UserGameItems VALUES (1, 5)
+
+SELECT * FROM UserGameItems WHERE ItemId = 1
+
+SELECT [UGI].[ItemId]
+	  ,[UGI].[UserGameId]
+	  ,[I].[MinLevel] AS [ItemLevel]
+	  ,[UG].[Level] AS [CharacterLevel]
+  FROM [UserGameItems] AS [UGI]
+  JOIN [Items] AS [I]
+    ON [UGI].[ItemId] = [I].[Id]
+  JOIN [UsersGames] AS [UG]
+    ON [UGI].[UserGameId] = [UG].[Id]
+ WHERE ItemId = 1
+
+
+DROP TRIGGER tr_UserGameItemsUpdate
+GO
+
+BEGIN TRANSACTION
+
+SELECT *
+  FROM [UsersGames]
+ WHERE [UsersGames].[UserId] IN (SELECT [Id] FROM [Users] WHERE [Username] IN ('baleremuda', 'loosenoise', 'inguinalself', 'buildingdeltoid', 'monoxidecos'))
+   AND [UsersGames].[GameId] IN (SELECT [Id] FROM [Games] WHERE [Name] = 'bali')
+
+UPDATE [UsersGames]
+   SET [Cash] += 5000
+ WHERE [UsersGames].[UserId] IN (SELECT [Id] FROM [Users] WHERE [Username] IN ('baleremuda', 'loosenoise', 'inguinalself', 'buildingdeltoid', 'monoxidecos'))
+   AND [UsersGames].[GameId] IN (SELECT [Id] FROM [Games] WHERE [Name] = 'bali')
+
+SELECT *
+  FROM [UsersGames]
+ WHERE [UsersGames].[UserId] IN (SELECT [Id] FROM [Users] WHERE [Username] IN ('baleremuda', 'loosenoise', 'inguinalself', 'buildingdeltoid', 'monoxidecos'))
+   AND [UsersGames].[GameId] IN (SELECT [Id] FROM [Games] WHERE [Name] = 'bali')
+
+ROLLBACK
+GO
+
+BEGIN TRANSACTION
+DECLARE @ItemId AS INT
+DECLARE @UserGameId AS INT
+SET @ItemId = 251;
+
+CREATE TABLE #NewItems(
+	[ItemId] INT,
+	[UserGameId] INT
+)
+
+WHILE @ItemId <= 299
+BEGIN
+	INSERT INTO #NewItems
+	SELECT @ItemId AS [ItemId]
+		  ,[UsersGames].[Id] AS [UserGameId]
+	  FROM [UsersGames]
+	 WHERE [UsersGames].[UserId] IN (SELECT [Id] FROM [Users] WHERE [Username] IN ('baleremuda', 'loosenoise', 'inguinalself', 'buildingdeltoid', 'monoxidecos'))
+	   AND [UsersGames].[GameId] IN (SELECT [Id] FROM [Games] WHERE [Name] = 'bali')	
+
+	SET @ItemId += 1;
+END;
+
+SET @ItemId = 501;
+
+WHILE @ItemId <= 539
+BEGIN
+	INSERT INTO #NewItems
+	SELECT @ItemId AS [ItemId]
+		  ,[UsersGames].[Id] AS [UserGameId]
+	  FROM [UsersGames]
+	 WHERE [UsersGames].[UserId] IN (SELECT [Id] FROM [Users] WHERE [Username] IN ('baleremuda', 'loosenoise', 'inguinalself', 'buildingdeltoid', 'monoxidecos'))
+	   AND [UsersGames].[GameId] IN (SELECT [Id] FROM [Games] WHERE [Name] = 'bali')	
+
+	SET @ItemId += 1;
+END;
+
+SELECT * 
+  FROM [UsersGames]
+ WHERE [UsersGames].[UserId] IN (SELECT [Id] FROM [Users] WHERE [Username] IN ('baleremuda', 'loosenoise', 'inguinalself', 'buildingdeltoid', 'monoxidecos'))
+   AND [UsersGames].[GameId] IN (SELECT [Id] FROM [Games] WHERE [Name] = 'bali')	
+
+UPDATE [UsersGames]
+   SET [Cash] -= (SELECT SUM(Price) AS [SUM]
+					FROM Items
+				   WHERE Items.Id IN (SELECT DISTINCT ItemId FROM #NewItems))
+ WHERE [Id] IN (SELECT DISTINCT UserGameId FROM #NewItems)
+
+ INSERT INTO [UserGameItems]
+ SELECT * FROM [#NewItems]
+
+DROP TABLE #NewItems
+
+SELECT * 
+  FROM [UsersGames]
+ WHERE [UsersGames].[UserId] IN (SELECT [Id] FROM [Users] WHERE [Username] IN ('baleremuda', 'loosenoise', 'inguinalself', 'buildingdeltoid', 'monoxidecos'))
+   AND [UsersGames].[GameId] IN (SELECT [Id] FROM [Games] WHERE [Name] = 'bali')	
+ROLLBACK
+GO
+
+   SELECT [U].[Username]
+    	 ,[G].[Name]
+    	 ,[UG].[Cash]
+    	 ,[I].[Name]
+     FROM [Users] AS [U]
+     JOIN [UsersGames] AS [UG]
+       ON [U].[Id] = [UG].[UserId]
+     JOIN [Games] AS [G]
+       ON [UG].[GameId] = [G].[Id]
+     JOIN [UserGameItems] AS [UGI]
+       ON [UG].[Id] = [UGI].[UserGameId]
+     JOIN [Items] AS [I]
+       ON [UGI].[ItemId] = [I].[Id]
+    WHERE [G].[Name] = 'Bali'
+ ORDER BY [U].[Username]
+         ,[I].[Name]
+GO
+
+-- Task 20
+DECLARE @StamatGameId INT
+SET @StamatGameId = (
+    SELECT Id 
+	  FROM [UsersGames]
+	 WHERE [UserId] = (SELECT [Id] FROM [Users] WHERE [Username] = 'Stamat')
+	   AND [GameId] = (SELECT [Id] FROM [Games] WHERE [Name] = 'Safflower')
+)
+
+DECLARE @RequiredAmount Money
+DECLARE @Cache_Amount Money
+
+SET @RequiredAmount = (SELECT SUM(Price)
+						    FROM [Items]
+						WHERE [MinLevel] >= 11
+							AND [MinLevel] <= 12
+							AND [Id] NOT IN (SELECT ItemId 
+											    FROM [UserGameItems] AS [UGI]
+											WHERE [UserGameId] = @StamatGameId))
+
+SET @Cache_Amount = (SELECT Cash FROM UsersGames WHERE Id = @StamatGameId)
+
+IF @Cache_Amount >= @RequiredAmount
+BEGIN
+	BEGIN TRANSACTION
+		UPDATE UsersGames
+			SET Cash -= @RequiredAmount
+			WHERE Id = @StamatGameId
+
+			INSERT INTO UserGameItems
+			SELECT Id, @StamatGameId
+			FROM [Items]
+			WHERE [MinLevel] >= 11
+			AND [MinLevel] <= 12
+			AND [Id] NOT IN (SELECT ItemId 
+								FROM [UserGameItems] AS [UGI]
+								WHERE [UserGameId] = @StamatGameId)
+	COMMIT
+END
+
+SET @RequiredAmount = (SELECT SUM(Price)
+						FROM [Items]
+					WHERE [MinLevel] >= 19
+						AND [MinLevel] <= 21
+						AND [Id] NOT IN (SELECT ItemId 
+											FROM [UserGameItems] AS [UGI]
+										WHERE [UserGameId] = @StamatGameId))
+
+SET @Cache_Amount = (SELECT Cash FROM UsersGames WHERE Id = @StamatGameId)
+
+IF @Cache_Amount >= @RequiredAmount
+BEGIN
+	BEGIN TRANSACTION
+		UPDATE UsersGames
+			SET Cash -= @RequiredAmount
+			WHERE Id = @StamatGameId
+
+			INSERT INTO UserGameItems
+			SELECT Id, @StamatGameId
+			FROM [Items]
+			WHERE [MinLevel] >= 19
+			AND [MinLevel] <= 21
+			AND [Id] NOT IN (SELECT ItemId 
+								FROM [UserGameItems] AS [UGI]
+								WHERE [UserGameId] = @StamatGameId)
+	COMMIT
+END
+
+  SELECT [I].[Name] AS [Item Name]
+    FROM [UserGameItems] AS [UGI]
+    JOIN [Items] AS [I]
+      ON [UGI].[ItemId] = [I].[Id]
+   WHERE [UGI].[UserGameId] = @StamatGameId
+ORDER BY [I].[Name]
+
+-- Task 21
+USE SoftUni
+GO
+
+CREATE PROCEDURE usp_AssignProject(@emloyeeId INT, @projectID INT)
+AS
+	DECLARE @proectsCount INT
+	SET @proectsCount = (SELECT COUNT(*) 
+						   FROM [EmployeesProjects] AS [EP]
+						  WHERE EmployeeID = @emloyeeId)
+
+	IF @proectsCount >= 3
+	BEGIN
+		RAISERROR ('The employee has too many projects!', 16, 1)
+		RETURN
+	END
+
+	INSERT INTO EmployeesProjects
+	     VALUES (@emloyeeId, @projectID)
+GO
+
+DROP PROCEDURE usp_AssignProject
+GO
+
+-- Task 22
+CREATE TABLE Deleted_Employees(
+	EmployeeId INT PRIMARY KEY,
+	FirstName VARCHAR(50),
+	LastName VARCHAR(50),
+	MiddleName VARCHAR(50),
+	JobTitle VARCHAR(50),
+	DepartmentId INT,
+	Salary MONEY
+)
+
+CREATE TRIGGER tr_EmployeesDelete ON Employees FOR DELETE
+AS
+	INSERT INTO Deleted_Employees(FirstName, LastName, MiddleName, JobTitle, DepartmentId, Salary)
+	SELECT FirstName
+		  ,LastName
+		  ,MiddleName
+		  ,JobTitle
+		  ,DepartmentId
+		  ,Salary
+	 FROM deleted
+GO
